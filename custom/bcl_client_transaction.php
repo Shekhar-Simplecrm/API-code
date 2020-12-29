8eaa7a99e7db5f30f5ecb8baa63eb6f3ffdc78e7
Shekhar-Simplecrm <shekhar@simplecrm.com>
2020-12-29 14:53:15 +0530

    new working api code

--- custom/bcl_client_transaction.php
+++ custom/bcl_client_transaction.php
@@ -2,101 +2,86 @@
 
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 /**
- * 
+ * Class to get Bajaj capital client transaction report.
  */
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
-
-
 class bcl_clientTransaction
 {
-	public function create_request($url, $post_fields, $headers)
+	/*
+	* Function to make a http request.
+	* @param string $url,
+	* @param array $post_fields,
+	* @param array $headers
+	*/
+	private function create_request($url, $post_fields, $headers)
 	{
-		$curl = curl_init($url);
-		// curl_setopt($curl, CURLOPT_URL, $url);
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($curl, CURLOPT_POST, 1);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $post_fields);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-		if(!empty($post_fields)) {
-			curl_setopt($curl, CURLOPT_POST, 1);
-			curl_setopt($curl, CURLOPT_POSTFIELDS, $post_fields);
-		}
-		
-		if(!empty($headers)) {
-			curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-		}
+		$result = curl_exec($curl);
 
-		$data = curl_exec($curl);
-		
+		$curl_error = curl_error($curl);
+		if($curl_error){
+			echo $curl_error;
+		}
 		curl_close($curl);
 
-		$json_data = json_decode($data);
+		$data = json_decode($result);
 
-		if($json_data->status == 'Success') {
-
-			return $json_data->Data;
-
-		} else {
-			return false;
+		if($data->status == 'Success') {
+			return $data;
 		}
+		
 	}
 
+	/*
+	* Function to get Client token from CRM.
+	* @response string (client token)
+	*/
 	public function getClientToken()
 	{
 		$token_url = "https://portfolio.wealthmaker.in/wmapi/WealthMakerAPIKit.svc/GenerateToken";
 
-		$headers = ['Content-Type' => 'application/json'];
+		$headers = ['Content-Type: application/json'];
 
-		$post_fields = [
+		$post_fields = json_encode([
 			"Source"   => "12",
 			"UserId"   => "scrmuser",
 			"Password" => "3pSw(cRm"
-		];
-
-		$request_data = $this->create_request($token_url, $post_fields, $headers);
-
-		if($request_data != false ) {
-
-			return $request_data[0]->Token;
-
-		} else {
-
-			$response = [
-				"status" => "Fail",
-				"message" => "Request not processed"
-			]
-			return json_encode($response);
-
-		}
+		]);
 		
+		$token_data = $this->create_request($token_url, $post_fields, $headers);
+		
+		return $token_data->Data[0]->Token;
 	}
-
+	/*
+	* Function to get client transaction report data.
+	* @response array.
+	*/
 	public function getClientTransactionReport()
 	{
+		// $client_token = "B7985C06444A53EFE055000000000001";
 		$client_token = $this->getClientToken();
 
 		$url = "https://portfolio.wealthmaker.in/wmapi/WealthMakerAPIKit.svc/ClientTransactionReport";
+		
+		$post_data = json_encode([
+			"token" => $client_token,
+			"client_code" => "40802547"
+		]);
 
-		$headers = ['Content-Type' => 'application/json'];
+		$headers = ['Content-Type: application/json'];
 
-		$post_fields = [
-			"token": $client_token,
-    		"client_code":"40802547"
-		];
-
-		$request_data = $this->create_request($url, $post_fields, $headers);
-
-		if($request_data != false ) {
-
-			return $request_data;
-
-		} else {
-
-			$response = [
-				"status" => "Fail",
-				"message" => "Request not processed"
-			]
-			return json_encode($response);
+		$request_data = $this->create_request($url, $post_data, $headers);
+		// echo "<pre>";
+		// echo print_r($request_data);
+		if($request_data->status == "Success" && empty($request_data->error)) {
+			return $request_data->Data;
 		}
 	}
 }
